1af1ba706dfd7568c8f033f217cef648350c8e0e
Prasanth P <prasanthxyz@gmail.com>
2016-01-09 21:28:58 +0530

    PATCH done

--- food_suggestion/urls.py
+++ food_suggestion/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'food_suggestion.views.test_drf', name='home'),
+    url(r'^get_all_items/$', 'food_suggestion.views.get_all_items', name='all_items'),
+    url(r'^get_item/(?P<item_id>\d+)/$', 'food_suggestion.views.get_item', name='item'),
 ]
--- food_suggestion/views.py
+++ food_suggestion/views.py
@@ -8,13 +8,28 @@ from food_suggestion.models import Item
 
 
 @api_view(['GET', 'POST'])
-def test_drf(request):
+def get_all_items(request):
     if request.method == 'GET':
         s = Item.objects.all()
         ser = ItemSerializer(s, many=True)
         return Response(ser.data)
     elif request.method == 'POST':
         ser = ItemSerializer(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=status.HTTP_201_CREATED)
+        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PATCH'])
+def get_item(request, item_id):
+    if request.method == 'GET':
+        item = Item.objects.get(pk=item_id)
+        ser = ItemSerializer(item)
+        return Response(ser.data)
+    elif request.method == 'PATCH':
+        item = Item.objects.get(pk=item_id)
+        ser = ItemSerializer(item, data=request.data, partial=True)
         if ser.is_valid():
             ser.save()
             return Response(ser.data, status=status.HTTP_201_CREATED)
