2649f3f886b1c6dbf56b6138393668767b504ac7
Prasanth P <prasanthxyz@gmail.com>
2016-01-09 19:19:36 +0530

    Model Item and Admin

--- food_suggestion/admin.py
+++ food_suggestion/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from food_suggestion.models import Item
 
-# Register your models here.
+admin.site.register(Item)
--- food_suggestion/models.py
+++ food_suggestion/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Item(models.Model):
     hotel_name = models.CharField(max_length=100)
-    geolocation = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)      # bread, beverage
+    veg = models.BooleanField()
