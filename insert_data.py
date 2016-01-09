e2023ca7a3abe48df8d7fe8e9e40ddcfd19bb6a7
Prasanth P <prasanthxyz@gmail.com>
2016-01-09 20:54:42 +0530

    insert data and data.csv

---
+++ insert_data.py
@@ -0,0 +1,32 @@
+import os
+import csv
+import sys
+from random import random
+
+
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'theeta.settings'
+
+from food_suggestion.models import Item
+
+
+hotels = ['Ahotel', 'Bhotel', 'Chotel', 'Dhotel', 'Ehotel']
+
+for hotel in hotels:
+    reader = csv.DictReader(open('data.csv', 'rb'))
+    for record in reader:
+        # hotel_name = record['hotel_name']
+        hotel_name = hotel
+        name = record['name']
+        # rating = record['rating']
+        rating = int(random()*1000)%6
+        price = int(record['price']) + (int(random()*100)%10 - 5)
+        category = ''
+        veg = True
+        Item(hotel_name=hotel_name,
+                name=name,
+                rating=rating,
+                price=price,
+                category=category,
+                veg=veg).save()
+
