cd1823a4bea7ba01b740dbb0bd2cd7599ff7e143
Prasanth P <prasanthxyz@gmail.com>
2016-01-10 02:20:58 +0530

    Data changes

--- food_suggestion/models.py
+++ food_suggestion/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Item(models.Model):
     hotel_name = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     rating = models.IntegerField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100, blank=True)      # bread, beverage
+    category = models.CharField(max_length=100, blank=True)  # bread, beverage
     veg = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name + " : " + str(self.price)
